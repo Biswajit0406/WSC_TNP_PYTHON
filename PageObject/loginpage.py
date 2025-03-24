5f2910f62c3e9bd40470d5aeb203bc5a65246ce5
Biswajit Bhadra <biswajit.bhadra@soulunileaders.com>
2025-03-24 11:39:34 +0530

    conftest file updated

--- PageObject/loginpage.py
+++ PageObject/loginpage.py
@@ -10,7 +10,7 @@ class LoginPage(Base):
 
 
     # Web Elements (Using Locators)
-    LOGO = (By.XPATH, "//section[@class='for-login']//div[@class='page-card-heads']")
+    LOGO = (By.XPATH, "//section[@class='for-login']//div[@class='page-card-head']")
     USERNAME_FIELD = (By.XPATH, "//input[@id='login_email']")
     PASSWORD_FIELD = (By.XPATH, "//input[@id='login_password']")
     LOGIN_BUTTON = (By.XPATH, "//button[normalize-space()='Login']")
